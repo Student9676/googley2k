b4ec3f511ab7440ca4cdca9451ac5c2a7a644e77
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-04-10 14:53:02 -0400

    added reading functionality to search basr

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import googleLogo from './assets/google.png'
 import './App.css'
 
 function App() {
+  const [searchText, setSearchText] = useState('');
+  
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    // normal centering looks uncentered so i added a static margin. plz change it if you know how to make it dynamic/better
+    <div className="min-h-screen flex items-center justify-center mt-[-30px]"> 
       <div className="flex flex-col items-center gap-0.5">
         
         <div className='flex flex-col items-center gap-7'>
@@ -12,10 +15,17 @@ function App() {
         <p>Search 1,326,920,000 webpages</p>
         </div>
         
-        <input type="text" className="border border-black w-md"/>
+        <input value={searchText} 
+        onChange={(e)=>setSearchText(e.target.value)} 
+        type="text" 
+        className="border border-black w-md"/>
       
         <div className="flex items-center gap-0.5">
-          <button className="text-white">Google Search</button>
+          <button 
+          className="text-white" 
+          onClick={() => alert(`Searching for: ${searchText}`)}>
+            Google Search
+          </button>
           <button className="text-white">I'm Feeling Lucky</button>
         </div>
       
