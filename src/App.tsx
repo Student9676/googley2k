166644b2be04c0dc14fc26c88a28e884b0c29f03
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-04-10 14:31:07 -0400

    created basic homepage structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import googleLogo from './assets/google.png'
 import './App.css'
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <img src={googleLogo} alt="Google Logo" className="max-w-md"/>
+      <div className="flex flex-col items-center gap-0.5">
+        
+        <div className='flex flex-col items-center gap-7'>
+        <img src={googleLogo} alt="Google Logo" className="max-w-xs"/>
+        <p>Search 1,326,920,000 webpages</p>
+        </div>
+        
+        <input type="text" className="border border-black w-md"/>
+      
+        <div className="flex items-center gap-0.5">
+          <button className="text-white">Google Search</button>
+          <button className="text-white">I'm Feeling Lucky</button>
+        </div>
+      
       </div>
     </div>
   )
